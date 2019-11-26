797d36f674b1df2c5c1e5ecc4192e821215b1291
msokal <57919173+msokal@users.noreply.github.com>
2019-11-26 09:10:00 +0300

    Update Executor.java

--- src/Executor.java
+++ src/Executor.java
@@ -1,41 +1,46 @@
-import buses.SchoolBus1;
-import buspark.buspark;
-import buses.RegionBus1;
+import buses.SchoolBus;
+import buses.RegionBus;
 import details.Motor;
 import details.DieselMotor;
 import details.GasMotor;
 import Person.Driver;
 import Person.Controler;
-import buses.Capabilities;
+import buses.IntFC;
 
 
 public class Executor {
 
     public static void main(String[] args) {
         Driver driver1 = new Driver("Max", "Maximov", "A/B/C/D/E");
-        Controler controler1 = new Controler("Blue", "Vest", "econom");
+        Controler controler1 = new Controler("Blue", "Vest", "express");
         Motor motor1= new GasMotor(10);
-        Motor motor2= new DieselMotor(20);
+        Motor motor2= new DieselMotor(15);
 
-        SchoolBus1 schoolbus1 = new SchoolBus1("Maz ", "103", 2000, schoolbus1);
-        RegionBus1 regionbus1 = new RegionBus1("Maz", "107", 2005, regionbus1);
-        
+        SchoolBus schoolbus1 = new SchoolBus ("Maz ", "103", 2000, null, motor1);
+        RegionBus regionbus1 = new RegionBus("Maz", "107", 2005, motor2);
+        IntFC schoolbus2 = new SchoolBus ("Maz ", "103", 2000, null, motor1);
+        IntFC regionbus2 = new RegionBus1("Maz", "107", 2005, motor2);
+        schoolbus1.FuelConsumption();
+        regionbus1.FuelConsumption();
+      //  System.out.println(schoolbus1.getInfoBus());
+       // System.out.println(regionbus1.getInfoBus());
+        //
         schoolbus1.FuelConsumption();
         regionbus1.FuelConsumption();
         System.out.println(schoolbus1.getInfoBus());
         System.out.println(regionbus1.getInfoBus());
 
-       // Buspark buspark1 = new Buspark("buspark ¹2");
-
+       
+        System.out.println("Driver name:" + driver1.getFirstName());
         System.out.println(driver1.toString());
-        System.out.println("Driver first name:" + driver1.getFirstName());
+       
         System.out.println(controler1.toString());
-        System.out.println(schoolbus1.toString());
-        System.out.println("Fuel Consumption for School Bus= " + schoolbus1.getMotor().showFuelConsumption());
-        System.out.println("Fuel Consumption for Region Bus= " + regionbus1.getMotor().showFuelConsumption());
+       // System.out.println(schoolbus1.toString());
+       // System.out.println("Fuel Consumption for School Bus= " + schoolbus1.getMotor().showFuelConsumption());
+       // System.out.println("Fuel Consumption for Region Bus= " + regionbus1.getMotor().showFuelConsumption());
 
 
 
-       //System.out.println(buspark1.toString());
+       
     }
 }
